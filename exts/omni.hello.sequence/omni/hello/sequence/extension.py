c16e1b43335c5a04d8b4989840528b569b5dd0c8
unknown <ovlaunch1@NVIDIALAUNCHPAD.COM>
2024-10-19 15:40:14 -0400

    working on async simulation from MJ

--- exts/omni.hello.sequence/omni/hello/sequence/extension.py
+++ exts/omni.hello.sequence/omni/hello/sequence/extension.py
@@ -6,6 +6,9 @@ from pxr import Usd, Sdf, Tf, UsdGeom, UsdLux, Gf, UsdShade, Vt
 import omni.kit.viewport.utility as vu
 omni.kit.pipapi.install("GPUtil")
 import GPUtil
+import omni.timeline
+from .menu import MenuEntry
+
 
 def create_payload(usd_context: omni.usd.UsdContext, path_to: Sdf.Path, asset_path: str) -> Usd.Prim:
     omni.kit.commands.execute("CreatePayload",
@@ -24,44 +27,159 @@ def print_gpu_memory_unload(loop_val):
         for gpu in gpus:
             carb.log_warn(f"unload {loop_val}: {gpu.memoryUsed}MB / {gpu.memoryTotal}MB used")
 
-class SequenceExtension(omni.ext.IExt):
-    def on_startup(self, ext_id):
+
+# Define a callback function to handle timeline events
+# def on_timeline_events(event):
+# 	if event.type == omni.timeline.TimelineEventType.CURRENT_TIME_TICKED.value:
+# 		# Difference between the last current time and the new one
+# 		# Clamped to zero if it would be negative
+# 		dt = event.payload['dt']
+# 		# Retrieve the new current time
+# 		current_time = event.payload['currentTime']
+# 		carb.log_warn(f'Current time changed, dt = {dt}s, end of frame = {current_time}s')
+# 	if event.type == omni.timeline.TimelineEventType.PLAY.value:
+# 		carb.log_warn(f'Timeline is now playing')
+# 	if event.type == omni.timeline.TimelineEventType.STOP.value:
+# 		carb.log_warn(f'Timeline is now stopped')
+# 	if event.type == omni.timeline.TimelineEventType.PAUSE.value:
+# 		carb.log_warn(f'Timeline is now paused')
+# 	if event.type == omni.timeline.TimelineEventType.START_TIME_CHANGED.value:
+# 		start_time = event.payload['startTime']
+# 		carb.log_warn(f'Timeline has a new start time: {start_time}s')
+# 	# For more events, please consult the TimelineEventType documentation
+
+# def on_time_update(event):
+# 	# Frame duration (dt)
+# 	frame_duration = event.payload['dt']
+#     # End time of the frame
+# 	end_time_of_frame = event.payload['currentTime']
+
+# 	# Perform custom computations for the frame, such as simulation, animation, UI updates etc.
+
+
+# # Define event callbacks
+# def subsystem_A(event):
+#     if event.type == omni.timeline.TimelineEventType.CURRENT_TIME_TICKED.value: 
+#        tick = event.payload['tick']
+#        carb.log_warn(f'Subsystem A was executed, tick index is {tick}')
+
+# def subsystem_B(event):
+#     if event.type == omni.timeline.TimelineEventType.CURRENT_TIME_TICKED.value: 
+#        tick = event.payload['tick']
+#        carb.log_warn(f'Subsystem B was executed, tick index is {tick}')
+
+
+
+class Simulation:
+    def __init__(self):
+        carb.log_warn(f"Simulation Init")
+        self.step = 0
+        self.base_directory = "C:/Users/ovlaunch1/Downloads/ns_prod_converted/ns_00000_thd_"
+        self.base_directory2 = "C:/Users/ovlaunch1/Downloads/wn_prod_converted/wn_00000_thd_0"
+
         stage = omni.usd.get_context().get_stage()
         world_path = Sdf.Path("/World")
         default_prim = UsdGeom.Xform.Define(stage, world_path)
-
+       
         stage.SetDefaultPrim(default_prim.GetPrim())
-        base_directory = "C:/Users/ovlaunch1/Downloads/ns_prod_converted/ns_00000_thd_"
-        base_directory2 = "C:/Users/ovlaunch1/Downloads/wn_prod_converted/wn_00000_thd_0"
-        self._window = ui.Window("My Window", width=300, height=300)
-        with self._window.frame:
-            with ui.VStack():
-                label = ui.Label("")
-                
-                def on_click():
-                    j = 1
-                    label.text = f"start"
-                    for i in range(1, 5):
-                        context: omni.usd.UsdContext = omni.usd.get_context()
-                        path = f"{base_directory}{i:0>2}_stl.usd"
-                        payload_prim: Usd.Prim = create_payload(context, Sdf.Path("/World/payload_prim"), path)
-                        payload_prim.Load()
-                        context2: omni.usd.UsdContext = omni.usd.get_context()
-                        path2 = f"{base_directory2}{i}_stl.usd"
-                        payload_prim2: Usd.Prim = create_payload(context2, Sdf.Path("/World/payload_prim2"), path2)
-                        payload_prim2.Load()
-                        print_gpu_memory(str(i))
 
-                def on_reset():
-                    world_path = Sdf.Path("/World")
-                    for prim in stage.TraverseAll():
-                        prim.Unload()
-                        print_gpu_memory(str(prim))
+        self.start_time = 0
+        self.state = f"Uninitialized"
+
+    def run_step(self):
+        context: omni.usd.UsdContext = omni.usd.get_context()
+        path = f"{self.base_directory}{self.step:0>2}_stl.usd"
+        payload_prim: Usd.Prim = create_payload(context, Sdf.Path(f"/World/payload_prim_{self.step}"), path)
+        payload_prim.Load()
+        context2: omni.usd.UsdContext = omni.usd.get_context()
+        path2 = f"{self.base_directory2}{self.step}_stl.usd"
+        payload_prim2: Usd.Prim = create_payload(context2, Sdf.Path(f"/World/payload_prim2_{self.step}"), path2)
+        payload_prim2.Load()
+
+    async def async_on_update(self,dt):
+        carb.log_warn(f"Upate Time Step")
+        if self.state != f"Running":
+            return
+        if now.time() - self.start_time > 1000:
+            self.start_time = now.time()
+            self.run_step()
+            self.step = self.step + 1
+            
+    def start(self):
+        carb.log_warn(f"Simulation Start")
+        self.state = f"Running"
+        self.start_time = now.time() 
+        self.step = 0
+    def stop(self):
+        carb.log_warn(f"Simulation Stop")                        
+        self.state = f"Stopped"
+        
+    def close(self):
+        self.stop()
+        carb.log_warn(f"Simulation Closed")
+
+
+class MenuEntryDemoLLM(MenuEntry):
+    async def async_on_start(self):
+        self.label.text = f"Start"
+
+    async def async_on_update(self,dt):
+        if self.simulation:
+            await self.simulation.async_update(dt)
+
+    async def async_on_stop():
+        world_path = Sdf.Path("/World")
+        for prim in stage.TraverseAll():
+            prim.Unload()
+            print_gpu_memory(str(prim))
+
+    async def window_create_async(self):
+        self.simulation = Simulation()
+
+        # this is required to set self._window
+
+        self._window = ui.Window(self.tool_name, width=500,height=500)
+        with self._window.frame:
+            with ui.VStack(style=GLOBAL_STYLE):
+                self.label = ui.Label("Select Model", height=20)
 
                 with ui.HStack():
-                    ui.Button("Start", clicked_fn=on_click)
-                    ui.Button("Reset", clicked_fn=on_reset)
+                    ui.Button("Start", clicked_fn=on_start)
+                    ui.Button("Stop", clicked_fn=on_stop)
 
+                def on_start():
+                    asyncio.ensure_future(self.async_on_start())
+                        
+                def on_stop():
+                    asyncio.ensure_fiture(self.async_on_stop())
+                        
+
+
+class SequenceExtension(omni.ext.IExt):
+    def on_startup(self, ext_id):
+        self._windows = [MenuEntryDemoLLM('LLM Demo', 'SA Demos/LLM Demo')]
+
+        # App provides common event bus. It is event queue which is popped every update (frame).
+        self._bus = omni.kit.app.get_app().get_update_event_stream()
+        self._sub = self._bus.create_subscription_to_push(self.on_update, name=f"SequenceExtension OnUpdate")
 
     def on_shutdown(self):
-        print("[omni.hello.sequence] SequenceExtension shutdown")
+        self._bus = None
+        self._sub = None
+        for window in self._windows:
+            window.shutdown()
+        self._windows = None
+        # Explicitly shut down window
+        # Need callback for visibility change
+
+    # This event fires very frequently. A callback with long-running code will block the
+    # UI and make the app unresponsive.
+    def on_update(self, e: carb.events.IEvent):
+        if self._windows:  # if even init
+            for window in self._windows:
+                window.on_update(e.payload["dt"])
+
+
+
+
+
---
+++ exts/omni.hello.sequence/omni/hello/sequence/menu.py
@@ -0,0 +1,138 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+
+# use this to create a hot reloadable ui window, callable from a menu item
+'''
+
+Example usage:
+
+class MenuEntryDemoLLM(MenuEntry) :
+    async def window_create_async(self):
+        # this is called from the on menu clicked handler
+        # this is responsible for creating whatever gui.,
+        # this is required to set self._window
+
+        self._window = ui.Window(self.tool_name, width=500,height=500)
+        with self._window.frame:
+            with ui.VStack(style=GLOBAL_STYLE):
+                self.model_label = ui.Label("Select Model", height=20)
+
+
+
+class MyExtension(omni.ext.IExt):
+    def on_startup(self, ext_id):
+        self._windows = [MenuEntryDemoLLM('LLM Demo', 'SA Demos/LLM Demo')]
+
+    def on_shutdown(self):
+        for window in self._windows:
+            window.shutdown()
+            self._window = None
+
+'''
+
+
+
+
+
+import asyncio
+import carb
+
+import omni.ext
+import omni.ui as ui
+import omni.kit.ui  # for get_editor_menu
+
+from functools import partial
+
+
+def obj_shutdown(obj):
+    if not obj:
+        return
+    if hasattr(obj, 'shutdown') and callable(getattr(obj, 'shutdown')):
+        getattr(obj, 'shutdown')()
+
+
+
+
+class MenuEntry():
+    # UI Callbacks.  No long running events!
+    # these typicall should call async routines.
+    def on_window_closed(self):
+        self.window_close()
+
+    def on_menu_clicked(self, *_arg):
+        asyncio.ensure_future(self._window_create_async())
+
+    def on_update(self, delta: float):
+        # ignore if we are not initialized yet!  This can and will be called before __init__ is done.
+        if hasattr(self, '_window') and self._window:
+            if hasattr(self._window, 'update') and callable(getattr(self._window, 'update')):
+                self._window.update(delta)
+
+    def on_window_visibility_changed(self, visible: bool):
+        if not visible:
+            # go async, don't close self in our own callback!
+            asyncio.ensure_future(self.window_close_async())
+            self.window_close()
+
+    async def window_close_async(self):
+        # if in a callback, you should queue this and
+        # not call windows_close directly.
+        self.window_close()
+
+    async def window_create_async(self):
+        # this is called from the on menu clicked handler
+        # this is responsible for creating whatever gui.,
+        # you should override this with your own implementation.
+        pass
+
+    async def _window_create_async(self):
+        # calls descendent window create that performs standard setup.
+        await self.window_create_async()
+        if not self._window:
+            return
+        # Handles the change in visibility of the window gracefully
+        self._window.set_visibility_changed_fn(self.on_window_visibility_changed)
+
+        # , on_close = self.on_window_closed
+
+    def window_close(self):
+        # when window closes, we want to get rid of our pointer to it.
+        if self._window:
+            obj_shutdown(self._window)
+            self._window = None
+
+    def __init__(self, tool_name: str, menu_path: str):
+        self.simulation = None  # HACK  Should be done by descended class
+        self.tool_name = tool_name
+        carb.log_info(f"{self.tool_name} menu startup")
+        self.menus = omni.kit.ui.get_editor_menu().add_item(menu_path, self.on_menu_clicked, toggle=False, value=False)
+        self._window = None
+
+        # The ability to show the window if the system requires it.
+        # You use it in QuickLayout.
+        def _menu_show_window(menu, value):
+            asyncio.ensure_future(self._window_create_async())
+
+        ui.Workspace.set_show_window_fn(tool_name, partial(_menu_show_window, None))
+        # super().__init__(tool_name)
+
+    def shutdown(self):
+        carb.log_info(f"{self.tool_name} menu shutdown")
+        self.window_close()
+        self.menus = None
+
