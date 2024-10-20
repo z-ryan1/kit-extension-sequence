f4cea3cc0d95623da515c767694f1684bd5dae7d
unknown <ovlaunch1@NVIDIALAUNCHPAD.COM>
2024-10-20 17:28:42 -0400

    have time loop working

--- exts/omni.hello.sequence/omni/hello/sequence/extension.py
+++ exts/omni.hello.sequence/omni/hello/sequence/extension.py
@@ -10,6 +10,7 @@ import GPUtil # type: ignore
 import omni.timeline # type: ignore
 from .menu import MenuEntry
 import asyncio
+from typing import Union
 
 def create_payload(usd_context: omni.usd.UsdContext, path_to: Sdf.Path, asset_path: str) -> Usd.Prim:
     omni.kit.commands.execute("CreatePayload",
@@ -18,6 +19,8 @@ def create_payload(usd_context: omni.usd.UsdContext, path_to: Sdf.Path, asset_pa
         asset_path=asset_path, 
     )
     return usd_context.get_stage().GetPrimAtPath(path_to)
+
+
 
 def print_gpu_memory(loop_val):
         gpus = GPUtil.getGPUs()
@@ -36,26 +39,51 @@ class Simulation:
         self.state = f"Uninitialized"
         self.last_valid_frame = 5
 
+        self.path = None
+        self.path2 = None
+
+        self.old_payload_prim = None
+        self.payload_prim = None
+        self.old_payload_prim2 = None
+        self.payload_prim2 = None
+
         self.base_directory = "C:/Users/ovlaunch1/Downloads/ns_prod_converted/ns_00000_thd_"
         self.base_directory2 = "C:/Users/ovlaunch1/Downloads/wn_prod_converted/wn_00000_thd_0"
 
-        stage = omni.usd.get_context().get_stage()
+        self.stage = omni.usd.get_context().get_stage()
         world_path = Sdf.Path("/World")
-        default_prim = UsdGeom.Xform.Define(stage, world_path)
-        stage.SetDefaultPrim(default_prim.GetPrim())
+        default_prim = UsdGeom.Xform.Define(self.stage, world_path)
+        self.stage.SetDefaultPrim(default_prim.GetPrim())
 
     def getstate(self):
         return self.state
 
-    async def async_run_step(self):
+    async def async_run_step_load(self):
         context: omni.usd.UsdContext = omni.usd.get_context()
-        path = f"{self.base_directory}{self.step:0>2}_stl.usd"
-        payload_prim: Usd.Prim = create_payload(context, Sdf.Path(f"/World/payload_prim_{self.step}"), path)
-        payload_prim.Load()
+        self.path = f"{self.base_directory}{self.step:0>2}_stl.usd"
+        self.payload_prim: Usd.Prim = create_payload(context, Sdf.Path(f"/World/payload_prim_{self.step}"), self.path)
+        self.payload_prim.Load()
         context2: omni.usd.UsdContext = omni.usd.get_context()
-        path2 = f"{self.base_directory2}{self.step}_stl.usd"
-        payload_prim2: Usd.Prim = create_payload(context2, Sdf.Path(f"/World/payload_prim2_{self.step}"), path2)
-        payload_prim2.Load()
+        self.path2 = f"{self.base_directory2}{self.step}_stl.usd"
+        self.payload_prim2: Usd.Prim = create_payload(context2, Sdf.Path(f"/World/payload_prim2_{self.step}"), self.path2)
+        self.payload_prim2.Load()
+
+
+
+
+    async def async_run_step_unload(self):
+        #context: omni.usd.UsdContext = omni.usd.get_context()
+        #path = f"{self.base_directory}{self.step:0>2}_stl.usd"
+        self.payload_prim.Unload()
+        mystep = self.step - 1
+        carb.log_warn(f'Removing: /World/payload_prim_{mystep}') 
+        foo_prim_path = Sdf.Path(f"/World/payload_prim_{mystep}")
+        self.stage.RemovePrim(foo_prim_path)
+        #context2: omni.usd.UsdContext = omni.usd.get_context()
+        #path2 = f"{self.base_directory2}{self.step}_stl.usd"
+        self.payload_prim2.Unload()
+        foo_prim_path = Sdf.Path(f"/World/payload_prim2_{mystep}")
+        self.stage.RemovePrim(foo_prim_path)
 
     async def async_on_update(self,dt):
         # test
@@ -65,12 +93,19 @@ class Simulation:
         if self.step > self.last_valid_frame:
             # for now we are going to loop, for dev reasons
             # When we are done, switch to just return
+            await self.async_run_step_unload()
             self.step = 1
-            
-        if time.time() - self.start_time > 1.0:
+
+
+        time_difference = time.time() - self.start_time
+        if time_difference > 2.0:
+            if self.step > 1:
+                await self.async_run_step_unload()  # unloads the existing payload-prim
+            carb.log_warn(f"time_difference={time_difference}")
             self.start_time = time.time()
-            await self.async_run_step()
+            await self.async_run_step_load()
             self.step = self.step + 1
+
 
     def start(self):
         carb.log_warn(f"Simulation Start")
