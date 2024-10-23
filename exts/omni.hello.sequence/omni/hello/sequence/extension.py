0462873d24c3ddef2d30805903f69d2d9fd6db72
zoe <zryan@nvidia.com>
2024-10-23 11:42:18 -0700

    added camera movement

--- exts/omni.hello.sequence/omni/hello/sequence/extension.py
+++ exts/omni.hello.sequence/omni/hello/sequence/extension.py
@@ -2,36 +2,16 @@ import omni.ext
 import omni.ui as ui
 import carb
 import asyncio
-from pxr import Sdf, UsdGeom, UsdShade, Gf
-
-from pxr import Usd, Sdf, UsdGeom
-
-def create_perspective_35mm_camera(stage: Usd.Stage, prim_path: str="/World/MyPerspCam") -> UsdGeom.Camera:
-    camera_path = Sdf.Path(prim_path)
-    usd_camera: UsdGeom.Camera = UsdGeom.Camera.Define(stage, camera_path)
-    usd_camera.CreateProjectionAttr().Set(UsdGeom.Tokens.perspective)
-    usd_camera.CreateFocalLengthAttr().Set(35)
-    # Set a few other common attributes too.
-    usd_camera.CreateHorizontalApertureAttr().Set(20.955)
-    usd_camera.CreateVerticalApertureAttr().Set(15.2908)
-    usd_camera.CreateClippingRangeAttr().Set((0.1,100000))
-    translate_op = usd_camera.AddTranslateOp()
-    translate_op.Set(value=(123.43060339597056, 89.90841433124608, 1490.475525864984))
-
-    return usd_camera
-
+from pxr import Sdf, UsdGeom, Gf, UsdShade
 
 def create_material(stage, mat_name):
     """Create a red material."""
     material_path = f"/World/{mat_name}"
     
-    # Create a material
     material = UsdShade.Material.Define(stage, Sdf.Path(material_path))
     shader = UsdShade.Shader.Define(stage, Sdf.Path(material_path + "/Shader"))
     shader.CreateIdAttr("UsdPreviewSurface")
     shader.CreateInput("diffuseColor", Sdf.ValueTypeNames.Color3f).Set((1.0, 0.0, 0.0))  # Red color
-    
-    # Bind the shader to the material
     material.CreateSurfaceOutput().ConnectToSource(shader.CreateOutput('surface', Sdf.ValueTypeNames.Token))
     
     return material
@@ -46,31 +26,57 @@ def create_payload(context, prim_path, payload_path):
     visibility_attr.Set(UsdGeom.Tokens.invisible)
     return payload_prim
 
-async def smooth_transition(current_ns, current_wn, overlap_duration):
+def create_camera_on_startup(stage, camera_path="/World/MyCamera"):
+    """Create a camera on startup from a specific view."""
+    camera_prim = UsdGeom.Camera.Define(stage, camera_path)
+    camera_prim.CreateFocalLengthAttr().Set(50.0)
+    camera_prim.CreateHorizontalApertureAttr().Set(36.0)
+    camera_prim.CreateVerticalApertureAttr().Set(24.0)
+    camera_prim.CreateFStopAttr().Set(5.6)
+    camera_prim.CreateFocusDistanceAttr().Set(1000.0)
+    xform = UsdGeom.Xform(camera_prim.GetPrim())
+    ''''
+    This is where you can change the starting camera position
+    '''
+    pos = Gf.Vec3f(123, 126, 1100)
+    translate_op = xform.AddTranslateOp()
+
+    translate_op.Set(pos)
+
+    return camera_prim, translate_op
+
+def update_camera_position(stage, camera_path, target_prim, translate_op):
+    """Update the camera position to look at the target prim."""
+    target_pos = translate_op.Get()
+
+    # Set the camera position above the target
+    new_camera_pos = Gf.Vec3f(target_pos[0], target_pos[1], target_pos[2] + 10.0)
+    translate_op.Set(new_camera_pos)
+
+
+async def smooth_transition(current_ns, current_wn, overlap_duration, stage, camera_path, translate_op):
     """Transition from the current prim to the next prim."""
     if current_ns and current_wn:
         imageable_ns = UsdGeom.Imageable(current_ns)
         imageable_wn = UsdGeom.Imageable(current_wn)
 
-        # Make both visible
         if imageable_ns:
             imageable_ns.MakeVisible()
         if imageable_wn:
             imageable_wn.MakeVisible()
 
-        # Wait for the overlap duration
+        update_camera_position(stage, camera_path, current_ns, translate_op)
         await asyncio.sleep(overlap_duration)
 
-        # Optionally hide both after waiting
         if imageable_ns:
             imageable_ns.MakeInvisible()
         if imageable_wn:
             imageable_wn.MakeInvisible()
 
-async def sequential_visibility_change(prims_ns, prims_wn, overlap_duration):
+async def sequential_visibility_change(prims_ns, prims_wn, overlap_duration, stage, camera_path, translate_op):
     """Sequentially change visibility for a list of prims."""
     for current_ns, current_wn in zip(prims_ns, prims_wn):
-        await smooth_transition(current_ns, current_wn, overlap_duration)
+        await smooth_transition(current_ns, current_wn, overlap_duration, stage, camera_path, translate_op)
 
 class SequenceExtension(omni.ext.IExt):
     def on_startup(self, ext_id):
@@ -79,14 +85,15 @@ class SequenceExtension(omni.ext.IExt):
         default_prim = UsdGeom.Xform.Define(stage, world_path)
         stage.SetDefaultPrim(default_prim.GetPrim())
 
+        # Create a camera
+        camera_path = "/World/MyCamera"
+        self.camera, translate_op = create_camera_on_startup(stage, camera_path )
+
         base_directory_wn = "omniverse://localhost/Users/admin/std_res_prod/wn_prod_converted/wn_00000_thd_"
         base_directory_ns = "omniverse://localhost/Users/admin/std_res_prod/ns_prod_converted/ns_00000_thd_"
 
         self.loaded_prims_wn = [] 
         self.loaded_prims_ns = [] 
-
-        cam_path = default_prim.GetPath().AppendPath("MyPerspCam")
-        camera = create_perspective_35mm_camera(stage, cam_path)
 
         self._window = ui.Window("My Window", width=300, height=300)
         with self._window.frame:
@@ -100,7 +107,7 @@ class SequenceExtension(omni.ext.IExt):
                     # Create a red material for NS prims
                     red_material = create_material(context.get_stage(), "RedMaterial")
 
-                    for i in range(1, 6):
+                    for i in range(1, 32):
                         val = f"{i:02}"
                         path_wn = f"{base_directory_wn}{val}_stl.usd"
                         prim_path_wn = f"/World/payload_prim_wn_{val}"
@@ -114,18 +121,17 @@ class SequenceExtension(omni.ext.IExt):
 
                         node_path = prim_path_ns + "/node_/mesh_"
                         ns_prim = context.get_stage().GetPrimAtPath(Sdf.Path(node_path))
+                        
                         material_binding_api = UsdShade.MaterialBindingAPI(ns_prim)
-
                         material_binding_api.Bind(red_material)
 
-                        if len(self.loaded_prims_wn) % 5 == 0:  # Load 5 prims at a time.
-                            await asyncio.sleep(0.25)  # Simulate loading delay.
+                        if len(self.loaded_prims_wn) % 5 == 0:
+                            await asyncio.sleep(0.25)
 
                     # Make the first prim visible after loading.
                     if self.loaded_prims_ns and self.loaded_prims_wn:
                         first_prim_ns = context.get_stage().GetPrimAtPath(self.loaded_prims_ns[0])
                         first_prim_wn = context.get_stage().GetPrimAtPath(self.loaded_prims_wn[0])
-
 
                         imageable_prim_ns = UsdGeom.Imageable(first_prim_ns)
                         if imageable_prim_ns:
@@ -142,7 +148,7 @@ class SequenceExtension(omni.ext.IExt):
                     prims_wn = [omni.usd.get_context().get_stage().GetPrimAtPath(path_wn) for path_wn in self.loaded_prims_wn]
                     prims_ns = [omni.usd.get_context().get_stage().GetPrimAtPath(path_ns) for path_ns in self.loaded_prims_ns]
 
-                    await sequential_visibility_change(prims_ns, prims_wn, 0.1)
+                    await sequential_visibility_change(prims_ns, prims_wn, 0.1, stage, camera_path, translate_op)
                     label.text = "All prims have been shown and hidden."
 
                 def call_async(fn):
